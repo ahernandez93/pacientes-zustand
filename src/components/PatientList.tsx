e3ede42b196ece40688ef6b857f1f43865da129e
AllanHernandez <allan_hc@outlook.com>
2025-06-18 00:10:08 -0600

    Hasta leccion 246

---
+++ src/components/PatientList.tsx
@@ -0,0 +1,13 @@
+import { usePatientStore } from "../store"
+
+export default function PatientList() {
+
+    const patients = usePatientStore(state => state.patients)
+    console.log(patients)
+
+    return (
+        <div>
+            Patient List
+        </div>
+    )
+}
